52c77d586bcbc8d064244669f4b9ab8e1b1360cb
BuildTools <davidmaamoaix@gmail.com>
2020-04-05 11:31:50 +0800

    Added Russian translation by fantom2010 (pushed from local). Thank you to fantom2010 for contribution.

--- src/main/java/cn/davidma/tinymobfarm/client/ClientEventHandler.java
+++ src/main/java/cn/davidma/tinymobfarm/client/ClientEventHandler.java
@@ -5,6 +5,8 @@ import cn.davidma.tinymobfarm.client.render.RenderMobFarm;
 import cn.davidma.tinymobfarm.common.TinyMobFarm;
 import cn.davidma.tinymobfarm.core.Reference;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -19,5 +21,8 @@ public class ClientEventHandler {
 	public static void clientSetup(FMLClientSetupEvent event) {
 		ScreenManager.registerFactory(TinyMobFarm.containerTypeMobFarm, GuiMobFarm::new);
 		ClientRegistry.bindTileEntityRenderer(TinyMobFarm.tileEntityMobFarm, RenderMobFarm::new);
+		
+		TinyMobFarm.mobFarms.forEach(
+				farm -> RenderTypeLookup.setRenderLayer(farm, RenderType.func_228643_e_()));
 	}
 }
